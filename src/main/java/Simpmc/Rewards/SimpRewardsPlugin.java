391fd42a662f8e3bc1b35a9f845c627fe06c5eac
Minecraft0122 <168195378+Minecraft0122@users.noreply.github.com>
2026-06-03 23:18:23 +0800

    Add reload command and reset tasks to plugin

--- src/main/java/Simpmc/Rewards/SimpRewardsPlugin.java
+++ src/main/java/Simpmc/Rewards/SimpRewardsPlugin.java
@@ -1,11 +1,15 @@
 package Simpmc.Rewards;
 
+import Simpmc.Rewards.command.ReloadCommand;
 import Simpmc.Rewards.command.RewardCommand;
+import Simpmc.Rewards.config.ConfigManager;
 import Simpmc.Rewards.data.DataManager;
 import Simpmc.Rewards.listener.InventoryListener;
 import Simpmc.Rewards.listener.JoinListener;
 import Simpmc.Rewards.listener.QuitListener;
+import Simpmc.Rewards.task.DailyResetTask;
 import Simpmc.Rewards.task.OnlineTracker;
+import Simpmc.Rewards.task.WeeklyResetTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpRewardsPlugin extends JavaPlugin {
@@ -13,7 +17,10 @@ public class SimpRewardsPlugin extends JavaPlugin {
     private static SimpRewardsPlugin instance;
 
     private DataManager dataManager;
+
     private OnlineTracker onlineTracker;
+    private DailyResetTask dailyResetTask;
+    private WeeklyResetTask weeklyResetTask;
 
     @Override
     public void onEnable() {
@@ -22,12 +29,14 @@ public class SimpRewardsPlugin extends JavaPlugin {
 
         saveDefaultConfig();
 
+        ConfigManager.load();
+
         this.dataManager = new DataManager(this);
 
         dataManager.init();
 
         /*
-         * 注册监听器
+         * 监听器
          */
 
         getServer().getPluginManager().registerEvents(
@@ -46,20 +55,32 @@ public class SimpRewardsPlugin extends JavaPlugin {
         );
 
         /*
-         * 注册命令
+         * 命令
          */
 
         getCommand("reward").setExecutor(
                 new RewardCommand()
         );
 
+        getCommand("simprewardsreload").setExecutor(
+                new ReloadCommand()
+        );
+
         /*
-         * 在线统计任务
+         * 任务
          */
 
         this.onlineTracker = new OnlineTracker(this);
 
         onlineTracker.start();
+
+        this.dailyResetTask = new DailyResetTask(this);
+
+        dailyResetTask.start();
+
+        this.weeklyResetTask = new WeeklyResetTask(this);
+
+        weeklyResetTask.start();
 
         getLogger().info("SimpRewards enabled.");
     }
